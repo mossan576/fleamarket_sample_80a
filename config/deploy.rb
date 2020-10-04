c55f31751abf109ad14a305d4b180b4a503d8b30
mossan576 <k.naoya576@gmail.com>
2020-10-04 15:24:52 +0900

    fix database

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "3.14.1"
 
 set :application, "fleamarket_sample_80a"
-set :repo_url, "git@example.com:mossan576/fleamarket_sample_80a.git"
+set :repo_url, "git@github.com:mossan576/fleamarket_sample_80a.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
