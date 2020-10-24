c13ca0a7081c1b66a1d3928af7a199cc0e435cff
TakeshiYada <blessing.1016.action@gmail.com>
2020-10-24 20:02:01 +0900

    [add] #6 ユーザー新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :address, dependent: :destroy
+  accepts_nested_attributes_for :address
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
