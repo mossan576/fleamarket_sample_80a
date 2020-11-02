deb921042fd038504ab4f2b3eccac18f83262412
TakeshiYada <blessing.1016.action@gmail.com>
2020-11-03 00:27:28 +0900

    [update] #6 バリデーションの付与

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postcode, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :block, presence: true
 end
