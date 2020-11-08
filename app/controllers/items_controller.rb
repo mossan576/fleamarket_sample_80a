059256fb10c99a43e48a33f7672c143605df72d0
mossan576 <k.naoya576@gmail.com>
2020-11-08 22:59:18 +0900

    コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.create!(item_params)
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :status, :freight, :shipment_source, :ship_date, :price, :brand, :buyer_id, images: []).merge(user_id: "1", category_id: "1")
+  end
 end
