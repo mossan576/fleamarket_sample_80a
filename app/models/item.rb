99639fa0a03101ec5cd2083eff014154fa270a2c
mossan576 <k.naoya576@gmail.com>
2020-11-09 23:46:29 +0900

    バリデーション付与

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   has_many_attached :images
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :status
+    validates :freight
+    validates :shipment_source
+    validates :ship_date
+    validates :price
+    validates :images
+  end
 end
