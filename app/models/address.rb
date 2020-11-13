f4a55de24b6aa535cc2ebdc9327bb1204607b0aa
TakeshiYada <blessing.1016.action@gmail.com>
2020-11-13 23:58:08 +0900

    [add] #6 単体テストの実行

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :user
 
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
-  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :family_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
   validates :postcode, presence: true
   validates :prefecture, presence: true
   validates :city, presence: true
