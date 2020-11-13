fca3cabd848f5f15a5ed1d65d4b087bb2b008d1e
mossan576 <k.naoya576@gmail.com>
2020-11-13 23:40:31 +0900

    itemモデル修正
    ・アソシエーション追加
    ・バリデーション追加
    ・アクティブハッシュ追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
   has_many_attached :images
+  belongs_to :user
+  belongs_to :category
+  
   with_options presence: true do
     validates :name
     validates :description
@@ -9,6 +12,11 @@ class Item < ApplicationRecord
     validates :ship_date
     validates :price
     validates :images
+    validates :user_id
     validates :category_id
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  
 end
