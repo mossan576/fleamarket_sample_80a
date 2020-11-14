e4aee0095f19a6991e8e1fc6ce4d55e9664b6b3c
mossan576 <k.naoya576@gmail.com>
2020-11-14 19:09:05 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,23 @@ ActiveRecord::Schema.define(version: 2020_11_07_111315) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "postcode", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building_name", null: false
+    t.string "phone_number", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
@@ -51,23 +68,12 @@ ActiveRecord::Schema.define(version: 2020_11_07_111315) do
     t.string "brand"
     t.string "size"
     t.integer "buyer_id"
-ActiveRecord::Schema.define(version: 2020_10_21_212835) do
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "postcode", null: false
-    t.string "prefecture", null: false
-    t.string "city", null: false
-    t.string "block", null: false
-    t.string "building_name", null: false
-    t.string "phone_number", null: false
     t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,7 +95,7 @@ ActiveRecord::Schema.define(version: 2020_10_21_212835) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "users"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
-  add_foreign_key "addresses", "users"
 end
