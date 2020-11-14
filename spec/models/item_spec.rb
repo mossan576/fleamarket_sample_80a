cbba064db37afae3b1d9834fad309cb122a3b0fd
mossan576 <k.naoya576@gmail.com>
2020-11-14 19:52:21 +0900

    Update item_spec.rb
    ・英語→日本語

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,63 +6,63 @@ describe Item do
     it "is invalid without a name" do
       item = build(:item, name: "")
       item.valid?
-      expect(item.errors[:name]).to include("can't be blank")
+      expect(item.errors[:name]).to include("を入力してください")
     end
 
     # 2. descriptionが空では登録できないこと
     it "is invalid without a description" do
       item = build(:item, description: "")
       item.valid?
-      expect(item.errors[:description]).to include("can't be blank")
+      expect(item.errors[:description]).to include("を入力してください")
     end
 
     # 3. statusが空では登録できないこと
     it "is invalid without a status" do
       item = build(:item, status: "")
       item.valid?
-      expect(item.errors[:status]).to include("can't be blank")
+      expect(item.errors[:status]).to include("を入力してください")
     end
 
     # 4. freightが空では登録できないこと
     it "is invalid without a freight" do
       item = build(:item, freight: "")
       item.valid?
-      expect(item.errors[:freight]).to include("can't be blank")
+      expect(item.errors[:freight]).to include("を入力してください")
     end
 
     # 5. shipment_sourceが空では登録できないこと
     it "is invalid without a shipment_source" do
       item = build(:item, shipment_source: "")
       item.valid?
-      expect(item.errors[:shipment_source]).to include("can't be blank")
+      expect(item.errors[:shipment_source]).to include("を入力してください")
     end
 
     # 6. ship_dateが空では登録できないこと
     it "is invalid without a ship_date" do
       item = build(:item, ship_date: "")
       item.valid?
-      expect(item.errors[:ship_date]).to include("can't be blank")
+      expect(item.errors[:ship_date]).to include("を入力してください")
     end
 
     # 7. priceが空では登録できないこと
     it "is invalid without a price" do
       item = build(:item, price: "")
       item.valid?
-      expect(item.errors[:price]).to include("can't be blank")
+      expect(item.errors[:price]).to include("を入力してください")
     end
 
     # 8. user_idが空では登録できないこと
     it "is invalid without a user_id" do
       item = build(:item, user_id: "")
       item.valid?
-      expect(item.errors[:user_id]).to include("can't be blank")
+      expect(item.errors[:user_id]).to include("を入力してください")
     end
 
     # 9. category_idが空では登録できないこと
     it "is invalid without a category_id" do
       item = build(:item, category_id: "")
       item.valid?
-      expect(item.errors[:category_id]).to include("can't be blank")
+      expect(item.errors[:category_id]).to include("を入力してください")
     end
     
   end
