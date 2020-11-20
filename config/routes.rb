3c93ee341e56188a39ff7907dec8a6eeca333e97
kura1114 <pwmidvm5uyytrr5mjkn@yahoo.co.jp>
2020-11-20 22:43:34 +0900

    ビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # before
-  get 'items/index'
+  get 'items/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # after
-  root 'items#index'
+  root 'items#show'
+  resources :items, only: [:show]
 end
