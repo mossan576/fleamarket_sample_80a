b2a85b8bc984076b478de516ab0b3978edc29827
TakeshiYada <blessing.1016.action@gmail.com>
2020-11-21 22:41:13 +0900

    [add] #18 単体テストとバリデーション、クレカ情報確認画面の実装及び商品購入画面の機能追加

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,13 @@
 class TransactionsController < ApplicationController
   
   def buy
+    credit_card = CreditCard.where(user_id: current_user.id).first
+    if credit_card.blank?
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(credit_card.customer_id)
+      @default_credit_card_information = customer.cards.retrieve(credit_card.card_id)
+    end
   end
 
   def done
--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe CreditCard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe CreditCard do
+  describe '#create' do
+
+    it "card_idがない場合は登録できないこと" do
+      credit_card = build(:credit_card, card_id: "")
+      credit_card.valid?
+      expect(credit_card.errors[:card_id]).to include("を入力してください")
+    end
+
+    it "重複したcard_idが存在する場合登録できないこと" do
+      credit_card = create(:credit_card)
+      another_credit_card = build(:credit_card, card_id: credit_card.card_id)
+      another_credit_card.valid? 
+      expect(another_credit_card.errors[:card_id]).to include("はすでに存在します") 
+    end
+
+    it "customer_idがない場合は登録できないこと" do
+      credit_card = build(:credit_card, customer_id: "")
+      credit_card.valid?
+      expect(credit_card.errors[:customer_id]).to include("を入力してください")
+    end
+
+    it "重複したcustomer_idが存在する場合登録できないこと" do
+      credit_card = create(:credit_card)
+      another_credit_card = build(:credit_card, customer_id: credit_card.customer_id)
+      another_credit_card.valid? 
+      expect(another_credit_card.errors[:customer_id]).to include("はすでに存在します") 
+    end
+
+  end
 end
