e4ffc22b4092014228c83895673bd242ad43162f
TakeshiYada <blessing.1016.action@gmail.com>
2020-11-22 19:00:56 +0900

    [add] #18 商品購入機能の実装

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,10 +7,25 @@ class TransactionsController < ApplicationController
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(credit_card.customer_id)
       @default_credit_card_information = customer.cards.retrieve(credit_card.card_id)
+      @item = Item.find(1)
     end
   end
 
   def done
+    @item = Item.find(1)
+    card = current_user.credit_card
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp::Charge.create(
+    amount: @item.price, 
+    customer: card.customer_id, 
+    currency: 'jpy', 
+    )
+    if @item.update(buyer_id: current_user.id)
+      flash[:notice] = '購入しました。'
+    else
+      flash[:alert] = '購入に失敗しました。'
+      redirect_to controller: "transactions", action: 'buy'
+    end
   end
 
 end
