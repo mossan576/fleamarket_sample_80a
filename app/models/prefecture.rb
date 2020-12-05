1d8af99a5086806627a5396386559e3a578a4c92
mossan576 <k.naoya576@gmail.com>
2020-12-05 19:36:49 +0900

    Update prefecture.rb

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -11,7 +11,7 @@ class Prefecture < ActiveHash::Base
       {id: '静岡県', name: '静岡県'}, {id: '愛知県', name: '愛知県'}, {id: '三重県', name: '三重県'},
       {id: '滋賀県', name: '滋賀県'}, {id: '京都府', name: '京都府'}, {id: '大阪府', name: '大阪府'},
       {id: '兵庫県', name: '兵庫県'}, {id: '奈良県', name: '奈良県'}, {id: '和歌山県', name: '和歌山県'},
-      {id: '鳥取県', name: '鳥取県'}, {id: '島根県', name: '島根県'}, {id: 33, name: '岡山県'},
+      {id: '鳥取県', name: '鳥取県'}, {id: '島根県', name: '島根県'}, {id: '岡山県', name: '岡山県'},
       {id: '広島県', name: '広島県'}, {id: '山口県', name: '山口県'}, {id: '徳島県', name: '徳島県'},
       {id: '香川県', name: '香川県'}, {id: '愛媛県', name: '愛媛県'}, {id: '高知県', name: '高知県'},
       {id: '福岡県', name: '福岡県'}, {id: '佐賀県', name: '佐賀県'}, {id: '長崎県', name: '長崎県'},
