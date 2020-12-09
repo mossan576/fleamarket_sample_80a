eb8ab33cee5b029414c3987ca9206da682e0c107
mryou117 <68369600+mryou117@users.noreply.github.com>
2020-12-10 06:15:24 +0900

    販売価格のリアルタイム表示

--- app/assets/javascripts/word_counter.js
+++ app/assets/javascripts/word_counter.js
@@ -1,7 +1,23 @@
 $(function(){
   $('.sell-box__item__description-form').on('input', function(){
-      var cnt = $(this).val().length;
-      $('.numerator').text(cnt);
+    let cnt = $(this).val().length;
+    $('.numerator').text(cnt);
   });
   $('.sell-box__item__description-form').trigger('input');
+});
+
+
+$(function() {
+  $('.sell-box__price-area__form').on('input', function() {
+    let val = $(this).val();
+    $('.sell-box__commission__sum').text(Math.floor($(this).val() * 0.1));
+    $('.sell-box__profit__sum').text(Math.ceil($(this).val() * 0.9));
+    $('.sell-box__commission__sum').prepend('¥ ')
+    $('.sell-box__profit__sum').prepend('¥ ')
+    if(val == 0 ) {
+    $('.sell-box__commission__sum').text('ー');
+    $('.sell-box__profit__sum').text('ー');
+    }
+  });
+  $('.sell-box__price-area__form').trigger('input');
 });
